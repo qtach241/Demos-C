285c6008e08b61f6ae6e8da1c9a6f5b4af07294b
qtach241 <qtach241@gmail.com>
2016-03-14 10:59:12 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int execute_program(char *program)
+{
+    return -1;    
+}
+
+int main(int argc, char *argv[])
+{
+    if( argc == 2 )
+    {
+        printf("The argument supplied is %s\n", argv[1]);
+        if (execute_program(argv[1]) == 0)
+        {
+            printf("Program returned success!\n");
+        }
+        else
+        {
+            printf("Program returned failure\n");
+        }
+    }
+    else if( argc > 2 )
+    {
+        printf("Too many arguments supplied.\n");
+    }
+    else
+    {
+        printf("One argument expected.\n");
+    }
+}
