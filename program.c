b8217c76c8d343d8996e32006f7b2b4092cafddf
qtach241 <qtach241@gmail.com>
2016-03-16 12:55:53 -0500

    debug

--- program.c
+++ program.c
@@ -10,35 +10,26 @@ static key_t key_lut[] =
 
 int string_to_key(char *s)
 {
-    printf("string_to_key start\r\n");
     int i;
     for (i = 0; i < NUMKEYS; i++)
     {
-        key_t *key = key_lut + i*sizeof(key_t);
-        if (strcmp(key->key_string, s) == 0)
-            return key->key_value;
+        if (strcmp(key_lut[i].key_string, s) == 0)
+            return key_lut[i].key_value;
     }
-    printf("string_to_key exit\r\n");
     return BADKEY;
 }
 
 int execute_program(char *s)
 {
-    printf("execute_program\r\n");
     switch(string_to_key(s))
     {
     case outputPID:
-        printf("do_outputPID();\r\n");
-        //return do_outputPID();
-        break;
+        return do_outputPID();
     case simplefork:
-        printf("do_simplefork();\r\n");
-        //return do_simplefork();
-        break;
+        return do_simplefork();
     case BADKEY:
         printf("BADKEYr\n");
         return BADKEY;
-        break;
     }
     return -1;
 }
