358fbf14c20ceac3f620b085edc4ce259355def1
qtach241 <qtach241@gmail.com>
2016-03-17 15:23:55 -0500

    debug

--- helper.h
+++ helper.h
@@ -2,6 +2,6 @@
 #define HELPER_H
 
 extern int makeargv(const char *s, const char *delimiters, char ***argvp);
-extern int freemakeargv(char **argv);
+extern void freemakeargv(char **argv);
 
 #endif
